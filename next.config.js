f3bca0c1ee4ecc736cac09e2b4afa1c196ffecc8
personal <abhishekkalia.productdesign@gmail.com>
2023-03-09 18:09:34 -0500

    Add yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// next.config.js
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  assetPrefix: isProd ? "/portfolio/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
