9c20cc79374542daeefd0d66d5c49fa8d4d0bbf7
personal <abhishekkalia.productdesign@gmail.com>
2023-03-09 19:23:23 -0500

    Tailwind fix

--- next.config.js
+++ next.config.js
@@ -7,10 +7,3 @@ module.exports = nextConfig;
 
 // next.config.js
 const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  assetPrefix: isProd ? "/portfolio/" : "",
-  images: {
-    unoptimized: true,
-  },
-};
