75cdea53472c3ddade8f16516500321a024c610d
personal <abhishekkalia.productdesign@gmail.com>
2023-03-14 20:37:55 -0400

    Restart with template

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
-
-// next.config.js
-const isProd = process.env.NODE_ENV === "production";
